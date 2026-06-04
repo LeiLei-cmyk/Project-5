5a8af5c441b56d92d500ecd7ba5874b2874e3601
LeiLei-cmyk <narlolii42@gmail.com>
2026-06-04 11:37:44 +0200

    first level variation implementation
    + optimalization of scene changes

---
+++ Project #5/Assets/Scripts/WritingTheAnswers.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WritingTheAnswers : MonoBehaviour
+{
+    private string answer = "moons";
+    public Button yourbutton;
+    public GameObject NextButton;
+    public TMPro.TMP_InputField inputField;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Button button = yourbutton.GetComponent<Button>();
+        button.onClick.AddListener(TaskOnClick);
+    }
+
+    // Update is called once per frame
+    void TaskOnClick()
+    {
+        string guess = this.inputField.text;
+        string guess1 = guess.ToLower();
+
+        if (guess1 == answer)
+        {
+            this.inputField.text = "Correct!";
+            NextButton.SetActive(true);
+        }
+        else
+        {
+            this.inputField.text = "Try Again!";
+            NextButton.SetActive(false);
+        }
+    }
+}
